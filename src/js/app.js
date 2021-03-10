74ae29cbedefe3dbbb5152a4c5452ce8c6a3712b
MrMasimo <masimo91@mail.ru>
2021-03-10 22:14:48 +0300

    working witn app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,30 @@
 import '../css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import formUI from './views/forms';
+
+const coefPhysicalActivity = {
+  minimal: 1.2,
+  low: 1.38,
+  medium: 1.46,
+  high: 1.55,
+  extraHigh: 1.64,
+  hard: 1.73,
+  extraHard: 1.9,
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = formUI.formParametrs;
+  const formActivity = formUI.formPhysicalActivity;
+  formActivity.addEventListener('change', (e) => {
+    e.preventDefault();
+    getCoefActivity();
+  });
+
+});
+
+function getCoefActivity() {
+  const select = formUI.getChangeRadio();
+  console.log(select, coefPhysicalActivity[select]);
+}
+
--- src/js/views/forms.js
+++ src/js/views/forms.js
@@ -1,7 +1,34 @@
 class FormUI {
   constructor () {
-    this.formParametrs = document.forms['params'];
+    this.formParametrs = document.forms['parameters'];
+    this.formPhysicalActivity = document.forms['physical-activity'];
+    this.ageInput = document.getElementById('ages');
+    this.heightInput = document.getElementById('height');
+    this.weightInput = document.getElementById('weight');
   }
+
+  get form() {
+    return this.formParametrs;
+  }
+
+  get ageValue() {
+    return this.ageInput.value;
+  }
+
+  get heightValue() {
+    return this.heightInput.value;
+  }
+
+  get weightValue() {
+    return this.weightInput.value;
+  }
+
+  getChangeRadio() {
+    const selectedActivity = document.querySelector('input[type=radio]:checked');
+    return selectedActivity.value;
+  }
+
 }
+
 const formUI = new FormUI();
 export default formUI;
