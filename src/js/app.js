7b673eeec069b95a640f7dd6afe5bc39c71b5930
MrMasimo <masimo91@mail.ru>
2021-03-28 18:13:49 +0300

    stab. version 1

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,9 @@ import '../css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap";
 
-
 import formUI from './views/forms';
 import activityChange from './helpers/activity';
 import genderChange from './helpers/gender';
-
-// const rezCal = (weight*10 + height*6.25 - ages*5 - 161/5)*activity
 
 const coefPhysicalActivity = {
   minimal: 1.2,
@@ -17,26 +14,25 @@ const coefPhysicalActivity = {
   extraHigh: 1.64,
   hard: 1.73,
   extraHard: 1.9,
-}
+};
 const currentCoef = {
   activityCoef: 1.2,
   genderCoef: 5,
-}
+};
 const genderCoef = {
   male: 5,
   female: -161,
-
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const formGenders = formUI.formGender;
-  const form = formUI.form;
   const formActivity = formUI.formPhysicalActivity;
-  let activityCoef = 1.2;
-
+  const calcButton = formUI.calcButton;
+  const clearButton = formUI.clearButton;
 
   formGenders.addEventListener('click', (e) => {
     e.preventDefault();
+    console.log("gender");
     genderChange();
     const gender = getCoefGender();
     currentCoef.genderCoef = genderCoef[gender];
@@ -46,8 +42,32 @@ document.addEventListener("DOMContentLoaded", () => {
   formActivity.addEventListener('change', (e) => {
     e.preventDefault();
     activityChange(e.target);
-    currentCoef.activityCoef = getCoefActivity();
+    currentCoef.activityCoef = coefPhysicalActivity[getCoefActivity()];
   });
+
+  calcButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    formUI.removeTemplateRez();
+
+    const weight = formUI.weightValue;
+    const height = formUI.heightValue;
+    const age = formUI.ageValue;
+    const isInvalidForm = formUI.checkValidParametrs(height, weight, age);
+
+    const rezCal =
+      (weight * 10 + height * 6.25 - age * 5 + currentCoef.genderCoef) *
+      currentCoef.activityCoef;
+    if (!isInvalidForm)
+    formUI.createTemplateRez(rezCal);
+  });
+
+  clearButton.addEventListener("click", (e) => {
+      e.preventDefault();
+    formUI.removeTemplateRez();
+    formUI.removeInvalidTemplate();
+    formUI.resetForms();
+
+   });
 
 });
 
--- src/js/views/forms.js
+++ src/js/views/forms.js
@@ -1,15 +1,14 @@
 class FormUI {
   constructor() {
+    this.formCard = document.querySelector(".card-body");
     this.formGender = document.querySelector(".genders");
     this.formParametrs = document.forms["parameters"];
     this.formPhysicalActivity = document.forms["physical-activity"];
-    this.ageInput = document.getElementById("ages");
+    this.ageInput = document.getElementById("age");
     this.heightInput = document.getElementById("height");
     this.weightInput = document.getElementById("weight");
-  }
-
-  get form() {
-    return this.formParametrs;
+    this.calcButton = document.querySelector(".button-count");
+    this.clearButton = document.querySelector(".button-clear");
   }
 
   get ageValue() {
@@ -24,6 +23,33 @@ class FormUI {
     return this.weightInput.value;
   }
 
+  checkValidParametrs(height, weight, age) {
+    let invalidCoef = 0;
+    if (age < 14 || age > 120) invalidCoef += 1;
+    if (height < 80 || height > 280) invalidCoef += 1;
+    if (weight < 30 || weight > 600) invalidCoef += 1;
+
+    this.removeInvalidTemplate();
+
+    if (invalidCoef)
+      this.showInvalidTemplate();
+
+    return invalidCoef;
+  }
+
+  showInvalidTemplate() {
+    const invalidTemplate = `
+      <p class="data-invalid ml-2 text-danger">
+        Пожалуйста введите реальные параметры
+      </p>`;
+    this.formCard.insertAdjacentHTML("beforeend", invalidTemplate);
+  }
+
+  removeInvalidTemplate() {
+    const invalidTemplate = document.querySelector(".data-invalid");
+    if (invalidTemplate) invalidTemplate.remove();
+  }
+
   getCheckedRadio() {
     const selectedActivity = document.querySelector(
       "input[class=custom-control-input]:checked"
@@ -33,7 +59,38 @@ class FormUI {
 
   getActiveButton() {
     const selectedActivity = document.querySelector(".active");
-    return selectedActivity.getAttribute('id');
+    return selectedActivity.getAttribute("id");
+  }
+
+  createTemplateRez(rez = 0) {
+
+    const template = `
+    <div class="card rezult-card mb-2 mx-auto w-50 border-dark">
+      <div class="card-body">
+        <h4 class="card-title center">Результат</h4>
+        <div class="card-text row">
+          <div class="d-flex flex-column col-md-4"> Похудение: <span class="font-weight-bold">${Math.round(
+            0.9 * rez
+          )} kkal</span></div>
+          <div class="d-flex flex-column col-md-4"> Поддержание веса: <span class="font-weight-bold">${Math.round(
+            rez
+          )} kkal</span></div>
+          <div class="d-flex flex-column col-md-4"> Набор массы: <span class="font-weight-bold">${Math.round(
+            1.1 * rez
+          )} kkal</span></div>
+        </div>
+      </div>
+    </div>`;
+    document.body.insertAdjacentHTML("afterend", template);
+  }
+
+  removeTemplateRez() {
+    const templateRezult = document.querySelector(".rezult-card");
+    if (templateRezult) templateRezult.remove();
+  }
+
+  resetForms() {
+    this.formParametrs.reset();
   }
 }
 
