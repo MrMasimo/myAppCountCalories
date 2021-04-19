72dc08e303f08465c145d910a042a3b70a0a5d05
MrMasimo <masimo91@mail.ru>
2021-04-19 15:20:33 +0300

    vers. 2

--- src/js/helpers/activity.js
+++
@@ -1,8 +0,0 @@
-function activityChange(id) {
-  const formsActivity = document.querySelectorAll(".custom-control-input");
-  formsActivity.forEach((form) => {
-    if ((form !== id) & form.checked) form.checked = false;
-  });
-}
-
-export default activityChange;
